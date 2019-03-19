deb9eebe68417197ebd19c3245ead55647007c0d
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-03-19 11:55:19 -0400

    bidirectional - lstm - gru

--- DeepLearningR/Ch06_bidirectional_lstm_imdb.R
+++ DeepLearningR/Ch06_bidirectional_lstm_imdb.R
@@ -6,20 +6,25 @@ k_clear_session()
 library(keras)
 max_features <- 10000  # Number of words to consider as features
 maxlen <- 500          # Cut texts after this number of words 
+
 # (among top max_features most common words)
 # Load data
 imdb <- dataset_imdb(num_words = max_features)
 c(c(x_train, y_train), c(x_test, y_test)) %<-% imdb
+
 # Reverse sequences
 x_train <- lapply(x_train, rev) # reverse seq
 x_test <- lapply(x_test, rev) # reverse seq
+
 # Pad sequences
 x_train <- pad_sequences(x_train, maxlen = maxlen)
 x_test <- pad_sequences(x_test, maxlen = maxlen)
+
 model <- keras_model_sequential() %>% 
   layer_embedding(input_dim = max_features, output_dim = 128) %>% 
   layer_lstm(units = 32) %>% 
   layer_dense(units = 1, activation = "sigmoid")
+
 model %>% compile(
   optimizer = "rmsprop",
   loss = "binary_crossentropy",
@@ -34,8 +39,10 @@ history <- model %>% fit(
 )
 
 plot(history)
+# acc 0.875 | overfitting
 
 k_clear_session()
+
 
 # --- bidirectional lstm --- #
 model <- keras_model_sequential() %>% 
@@ -44,11 +51,13 @@ model <- keras_model_sequential() %>%
     layer_lstm(units = 32)
   ) %>% 
   layer_dense(units = 1, activation = "sigmoid")
+
 model %>% compile(
   optimizer = "rmsprop",
   loss = "binary_crossentropy",
   metrics = c("acc")
 )
+
 history <- model %>% fit(
   x_train, y_train,
   epochs = 10,
@@ -57,3 +66,4 @@ history <- model %>% fit(
 )
 
 plot(history)
+# observation 0.875 similar to last one above
